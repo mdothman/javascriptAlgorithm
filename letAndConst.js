c33ac14d96e60cb14479f5cbfa09a039152fc868
BEELZ <othmanmuhidean@gmail.com>
2020-11-17 11:35:30 -0800

    I know how to use let and const to capture vairables.

---
+++ letAndConst.js
@@ -0,0 +1,10 @@
+const letAndConst = (r) =>{
+    
+const PI = Math.PI;
+const area = PI*r*r
+const perimeter = PI*2*r
+console.log(area,perimeter)
+return area, perimeter
+}
+
+module.exports = letAndConst
--- test/loops.test.js
+++ test/loops.test.js
@@ -5,11 +5,8 @@ describe("loops",()=>{
     it("is a function",()=>{
         expect(vowelsAndConsonants).toEqual(vowelsAndConsonants)
     })
-    test("Returns array of values",()=>{
-        expect(vowelsAndConsonants("abc")).toEqual(["a","b","c"])
-    })
     test("the return values are in alphabetical order",()=>{
-        expect(vowelsAndConsonants("javascript")).toEqual(["a","a","i","j","v","s","c","r","p","t"])
+        expect(vowelsAndConsonants("javascript")).toEqual("jvscrptaai")
     })
     
 })
